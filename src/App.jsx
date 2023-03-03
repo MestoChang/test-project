131272df0a20b046f31b4c66eabae9ef6c666aec
Yenchen <k307849@gmail..com>
2023-03-03 14:02:16 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' exact element={<About />} />
+        <Route path='/projects' exact element={<Projects />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const About = () => {
+    return (
+        <div>
+            About
+            <Navbar/>
+        </div>
+    )
+}
+
+export default About
